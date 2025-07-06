3088c1b279bfc132e23183988e9a571406db8184
Rakshith Kumar B <rakshithkumab01718@gmail.com>
2025-07-06 08:50:52 +0530

    Add files via upload
    java basics

---
+++ variables-Datatypes/datatype.java
@@ -0,0 +1,26 @@
+
+
+public class datatype {
+    public static void main(String[] args) {
+        // Declare a variable of type int
+        int myInt = 10;
+        System.out.println("Integer value: " + myInt);
+
+        // Declare a variable of type double
+        double myDouble = 5.99;
+        System.out.println("Double value: " + myDouble);
+
+        // Declare a variable of type char
+        char myChar = 'A';
+        System.out.println("Character value: " + myChar);
+
+        // Declare a variable of type boolean
+        boolean myBoolean = true;
+        System.out.println("Boolean value: " + myBoolean);
+
+        // Declare a variable of type String
+        String myString = "Hello, World!";
+        System.out.println("String value: " + myString);
+    }
+    
+}
---
+++ variables-Datatypes/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+        // Declare a variable
+        int myNumber = 5;
+
+        // Print the variable
+        System.out.println("My number is: " + myNumber);
+
+        // Change the value of the variable
+        myNumber = 10;
+
+        // Print the new value of the variable
+        System.out.println("My number is now: " + myNumber);
+
+        // Declare a constant
+        final int MY_CONSTANT = 100;
+
+        // Print the constant
+        System.out.println("My constant is: " + MY_CONSTANT);
+    }
+}
